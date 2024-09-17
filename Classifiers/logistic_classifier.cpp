86f264a42930bb56682e4e0fee7bde81527a0cb3
rdk1199 <rdk1199@gmail.com>
2024-09-16 19:47:10 -0700

    Implemented BinaryLogisticClassifier

--- Classifiers/logistic_classifier.cpp
+++ Classifiers/logistic_classifier.cpp
@@ -4,13 +4,66 @@
 
 using std::vector;
 
-/*
 template<class S>
-vector<double> BinaryLogisticClassifer<S>::prob_vec(const vector<S>& in)
+BinaryLogisticClassifier<S>::BinaryLogisticClassifier(int dim, const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes):
+	_dim(dim)
 {
-	double p0 = logistic(weights * in + bias);
-	return { p0, 1 - p0 };
+	compute_params(tr_data, tr_classes);
 }
 
-*/
+template<class S>
+vector<double> BinaryLogisticClassifier<S>::prob_vec(const std::vector<S>& in)
+{
+	double p0 = logistic(weights * in);
+	return { p0, 1.0 - p0 };
+}
 
+template<class S>
+vector<S> BinaryLogisticClassifier<S>::compute_en_grad(const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes, const vector<S>& y)
+{
+	vector<S> out(tr_data[0].size(), 0.0);
+
+	for (int i = 0; i < tr_data.size(); i++)
+	{
+		out += (y[i] - tr_classes[i]) * tr_data[i];
+	}
+
+	return out;
+}
+
+template<class S>
+Matrix<S> BinaryLogisticClassifier<S>::compute_hessian(const Matrix<S>& tr_data_matrix, const vector<S>& y)
+{
+	Matrix<S> R(y.size(), y.size());
+
+	for (int i = 0; i < R.n_rows(); i++)
+	{
+		R[i][i] = y[i] * (1.0 - y[i]);
+	}
+
+	return tr_data_matrix.transpose() * R * tr_data_matrix;
+
+}
+
+template<class S> //
+void BinaryLogisticClassifier<S>::compute_params(const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes, int n_iter)
+{
+	Matrix<S> data_mat(tr_data);
+
+	for (int i = 0; i < n_iter; i++)
+	{
+		vector<S> y;
+		for (int i = 0; i < tr_data.size(); i++)
+		{
+			y.push_back(logistic(weights * tr_data[i]));
+		}
+
+		vector<S> en_grad = compute_en_grad(tr_data, tr_classes, y);
+		Matrix<S> hessian = compute_hessian(data_mat, y);
+
+		weights = weights - hessian.inverse() * en_grad;
+	}
+
+}
+
+template class BinaryLogisticClassifier<double>;
--- Classifiers/logistic_classifier.h
+++ Classifiers/logistic_classifier.h
@@ -2,9 +2,10 @@
 
 #include <vector>
 
+#include "../Math/linalg.h"
 
 template<class S>
-class BinaryLogisticClassifier
+class BinaryLogisticClassifier //notation based off Bishop (2006) sec 4.3.2
 {
 
 private:
@@ -12,16 +13,17 @@ private:
 	int _dim;
 
 	std::vector<S> weights;
-	S bias; 
+	//S bias; 
 
-	std::vector<double> _p_classes;
+	std::vector<S> compute_en_grad(const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes, const std::vector<S>& y);
+	Matrix<S> compute_hessian(const Matrix<S>& tr_data_matrix, const std::vector<S>& y);
 
-	void compute_params(const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes);
+	void compute_params(const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes, int n_iter = 10);
 
 public:
 
 	//constructor/trainer
-	BinaryLogisticClassifier(int dim, const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes, const std::vector<double>& p_classes = {});
+	BinaryLogisticClassifier(int dim, const std::vector<std::vector<S>>& tr_data, const std::vector<int>& tr_classes);
 	
 	//to classify new data
 	int operator()(const std::vector<S>& in); //classify input 
